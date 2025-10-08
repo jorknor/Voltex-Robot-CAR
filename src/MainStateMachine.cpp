3c8996c2193402ed4c2c2c5c603bfe9faf05fd1c
imbluedabr <baldergroen@gmail.com>
2025-10-08 19:41:59 +0200

    added statemachine code

--- src/MainStateMachine.cpp
+++ src/MainStateMachine.cpp
@@ -1,7 +1,26 @@
 #include "MainStateMachine.h"
 
-enum MainState currentState = Idle;
+
+//array of state function pointers
+void (*stateFunctions[6])(struct State*) = {0};
+
+struct State currentState = {
+    .id = Idle,
+    .currentEvent = None
+};
+
+//this is probably an unnecessary abstraction
+void registerNewState(enum StateID funcID, void (*newFunc)(struct State*)) {
+    stateFunctions[funcID] = newFunc;
+}
 
 
-
+void runCurrentState() {
+    void (*stateFunc)(struct State*) = stateFunctions[currentState.id];
+    
+    //check if the function pointer is valid
+    if (stateFunc != nullptr) {
+        stateFunc(&currentState);
+    }
+}
 
