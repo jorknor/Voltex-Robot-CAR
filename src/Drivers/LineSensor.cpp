80dd79a721b32764e58613b3cad4f2cfadd612dc
jorknor <jor123456789rit@gmail.com>
2025-10-24 14:20:21 +0200

    Update LineSensor.cpp

---
+++ src/Drivers/LineSensor.cpp
@@ -0,0 +1,12 @@
+void lineSensorInit(){
+  pinmode (HLS_PIN, input);
+  pinmode (MLS_PIN, input);
+  pinmode (MRS_PIN, input);
+  pinmode (HRS_PIN, input);
+}
+void lineSensorUpdate(){
+  lineSensors[0] = digitalRead(HLS_PIN);
+  lineSensors[1] = digitalRead(MLS_PIN);
+  lineSensors[2] = digitalRead(MRS_PIN);
+  lineSensors[3] = digitalRead(HLS_PIN);
+}
