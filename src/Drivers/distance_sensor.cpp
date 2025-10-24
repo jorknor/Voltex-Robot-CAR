e08afbd990a297ca66e28469120d741f80a7f6ba
kletskid <ad.gerritsen2@student.han.nl>
2025-10-24 13:03:43 +0200

    Create distance_sensor.cpp

---
+++ src/Drivers/distance_sensor.cpp
@@ -0,0 +1,16 @@
+#include <distance_sensor.h>
+unsigned int getDistanceLeft() {
+unsigned long leftDuration = pulseIn(leftEchoPin, HIGH, 10000);
+unsigned int leftDistance_cm = leftDuration * 0.034 / 2;
+return leftDistance_cm;
+}
+unsigned int getDistanceRight() {
+  unsigned long rightDuration = pulseIn(rightEchoPin, HIGH, 10000);
+unsigned int rightDistance_cm = rightDuration * 0.034 / 2;
+return rightDistance_cm;
+}
+unsigned int getDistanceMiddle() {
+  unsigned long middleDuration = pulseIn(middleEchoPin, HIGH, 10000);
+unsigned int middleDistance_cm = middleDuration * 0.034 / 2;
+return middleDistance_cm;
+}
