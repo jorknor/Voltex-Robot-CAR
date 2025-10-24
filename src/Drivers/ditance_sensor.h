eaf332eeb14470d217f203e30ee1d0c0b4e4a3fa
kletskid <ad.gerritsen2@student.han.nl>
2025-10-24 13:03:46 +0200

    Update and rename HC-SR04.h to ditance_sensor.h

---
+++ src/Drivers/ditance_sensor.h
@@ -0,0 +1,10 @@
+#pragma once
+#DEFINE leftTrigPin  D2
+#DEFINE leftEchoPin  D3
+#DEFINE rightTrigPin  D4
+#DEFINE rightEchoPin  D5
+#DEFINE middleTrigPin  D6
+#DEFINE middleEchoPin  D7
+unsigned int getDistanceLeft();
+unsigned int getDistanceMiddle();
+unsigned int getDistanceRight();
