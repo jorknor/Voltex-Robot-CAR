e0f31bc5381abca2d7fe70fd56cea184ca0f14e3
jorknor <jor123456789rit@gmail.com>
2025-10-24 14:23:37 +0200

    Update LineSensor.h

--- src/Drivers/LineSensor.h
+++ src/Drivers/LineSensor.h
@@ -1,9 +1,10 @@
-#define HLS A0
-#define MLS A1
-#define MRS A2
-#define HRS A3
+#define HLS_PIN A0
+#define MLS_PIN A1
+#define MRS_PIN A2
+#define HRS_PIN A3
 
 extern bool lineSensors[4]
 void lineSensorInit();
 void lineSensorUpdate();
+
 
