759ef51a3fa8d1678f849082fd88d3ed466ec8e6
Genarro <97437921+Genarro165@users.noreply.github.com>
2025-10-28 14:23:10 +0100

    Update robotcarcodetest1.ino
    EEN PAAR AANPASSINGEN

--- robotcarcodetest1.ino
+++ robotcarcodetest1.ino
@@ -3,7 +3,7 @@
 
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
-
+#include "Drivers/MoterDriver.h"
 // ---------- PINS ----------
 // Line sensors (4x)
 const int sensorLeft     = A0;
@@ -44,72 +44,15 @@ int getDistance();
 
 void setup() {
   Serial.begin(9600);
-
-  pinMode(sensorLeft, INPUT);
-  pinMode(sensorMidLeft, INPUT);
-  pinMode(sensorMidRight, INPUT);
-  pinMode(sensorRight, INPUT);
-
-  pinMode(ENA, OUTPUT); pinMode(IN1, OUTPUT); pinMode(IN2, OUTPUT);
-  pinMode(ENB, OUTPUT); pinMode(IN3, OUTPUT); pinMode(IN4, OUTPUT);
-
-  pinMode(trigPin, OUTPUT); pinMode(echoPin, INPUT);
-  pinMode(buzzer, OUTPUT);
-  pinMode(ledLeft, OUTPUT);
-  pinMode(ledRight, OUTPUT);
-
-  lcd.init();
-  lcd.backlight();
-
-  Serial.println("=== ROBOTCAR TEST MODE (4 sensors + slave) ===");
-}
-
+  setMoterState(FOWARDS);
+} 
+  
 void loop() {
   // ----- LINE SENSOR TEST -----
-  int l  = analogRead(sensorLeft);
-  int ml = analogRead(sensorMidLeft);
-  int mr = analogRead(sensorMidRight);
-  int r  = analogRead(sensorRight);
-
-  Serial.print("L: "); Serial.print(l);
-  Serial.print(" ML: "); Serial.print(ml);
-  Serial.print(" MR: "); Serial.print(mr);
-  Serial.print(" R: "); Serial.println(r);
-
-  // ----- ULTRASOON SENSOR -----
-  int dist = getDistance();
-  Serial.print("Ultrason distance: "); Serial.print(dist); Serial.println(" cm");
-
-  // ----- SLAVE + LINE FOLLOW SIMULATIE -----
-  if (dist > 15 || dist == 0) {  
-    // veilig -> lijn volgen
-    if (ml > 500 && mr > 500) {
-      forward(150, 150);  // midden -> rechtdoor
-    } else if (l > 500 || ml > 500) {
-      forward(100, 150);  // links -> corrigeer links
-      digitalWrite(ledLeft, HIGH);
-    } else if (r > 500 || mr > 500) {
-      forward(150, 100);  // rechts -> corrigeer rechts
-      digitalWrite(ledRight, HIGH);
-    } else {
-      stopMotors(); // geen lijn -> stoppen
-      blinkBuzzer(1);
-    }
-  } else {
-    // te dichtbij -> stoppen
-    stopMotors();
-    blinkBuzzer(2);
-    Serial.println("!!! STOP: obstakel !!!");
-  }
-
+  
+ 
   // ----- LCD TEST -----
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print("Line/Slave TEST");
-  lcd.setCursor(0, 1);
-  lcd.print("Dist:" + String(dist) + "cm");
-
-  delay(500); // korte pauze
+  
 }
 
 // ---------- MOTOR FUNCTIONS ----------
@@ -137,12 +80,5 @@ void blinkBuzzer(int times) {
   }
 }
 
-// ---------- ULTRASOON SENSOR ----------
-int getDistance() {
-  digitalWrite(trigPin, LOW); delayMicroseconds(2);
-  digitalWrite(trigPin, HIGH); delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
-  long duration = pulseIn(echoPin, HIGH, 20000);
-  if (duration == 0) return 0;
-  return duration * 0.034 / 2;
+
 }
