143a6b55e11b0abc3f01f0b0caa8731ef0a7b30f
jorknor <jor123456789rit@gmail.com>
2025-10-28 14:06:39 +0100

    Update Slave.cpp

--- src/States/Slave.cpp
+++ src/States/Slave.cpp
@@ -2,7 +2,27 @@
 #include "../Drivers/Linesensor.h"
 #include "../MainStateMachine.h"
 #include "../Drivers/MotorDriver.h"
+
 void slaveState(struct State* currState){
-
-
+  if (HLS_PIN == MLS_PIN == MRS_PIN == HRS_PIN){
+    setMotorState(FORWARD);
+  }
+  else if(MLS_PIN == TRUE){
+    setMotorState(LEFT);
+  }
+  else if(HLS_PIN == TRUE){
+    setMotorState(HARD_LEFT);
+  }
+  else if(MRS_PIN == TRUE){
+    setMotorState(RIGHT);
+  }
+  else if(HRS_PIN == TRUE){
+    setMotorState(HARD_RIGHT);
+  }
+  else{
+      if (currState->currentEvent == Slave) {
+            currentState->id = Idle;
+      }
+   }
 }
+
