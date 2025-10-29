2a46c20ad29645c999e5951067b4432ae61d23e2
kletskid <ad.gerritsen2@student.han.nl>
2025-10-29 10:54:23 +0100

    Update Idle.cpp
    added the header file

--- src/States/Idle.cpp
+++ src/States/Idle.cpp
@@ -1,7 +1,8 @@
 #include "../MainStateMachine.h"
 #include "../Drivers/MotorDriver.h"
-
+#include "Idle.h"
 void idleState(struct State* currState){
   setMotorState(HALTED);
 // insert (remote control om state van de robot te veranderen) here
 }
+
