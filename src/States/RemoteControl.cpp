91497fbe0401d379f00fea45d0d67a3a6873c10c
kletskid <ad.gerritsen2@student.han.nl>
2025-10-29 19:36:44 +0100

    Fix motor state function and include header

--- src/States/RemoteControl.cpp
+++ src/States/RemoteControl.cpp
@@ -1,11 +1,11 @@
-#include "RemoteConrtol.h"
+#include "RemoteControl.h"
 
 void remoteControlState(struct State* currState){
   if (currState->currentEvent == InputComplete){
     if (strncmp(inputBuffer[0], "w", 1) == 0){
-      setMotorState(FORWARD);
+      setMotorState(FORWARDS);
     } else if (strncmp(inputBuffer[0], "a", 1) == 0) {
-      setmMotorState(LEFT);
+      setMotorState(LEFT);
     } else if (strncmp(inputBuffer[0], "d", 1) == 0) {
       setMotorState(RIGHT);
     } else if (strncmp(inputBuffer[0], "s", 1) == 0) {
@@ -16,4 +16,5 @@ void remoteControlState(struct State* currState){
     flushArguments();
   }
 }
+
 
