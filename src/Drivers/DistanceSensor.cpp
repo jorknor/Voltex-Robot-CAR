95081adc4ea1286f66ce70eff0910463fbb201c8
kletskid <ad.gerritsen2@student.han.nl>
2025-10-29 18:39:16 +0100

    Refactor DistanceSensor.cpp for better organization
    Refactor distance sensor code to improve readability and structure.

--- src/Drivers/DistanceSensor.cpp
+++ src/Drivers/DistanceSensor.cpp
@@ -1,14 +1,28 @@
 #include "DistanceSensor.h"
 
-unsigned int leftDistance_cm;
-unsigned int rightDistance_cm;
-unsigned int middleDistance_cm;
-
-void distanceUpdate(){
- unsigned long leftDuration = pulseIn(leftEchoPin, HIGH, 10000);
+int leftDistance_cm;
+int rightDistance_cm;
+int middleDistance_cm;
+void distanceSensorInit(){
+ pinMode(leftEchoPin, INPUT);
+ pinMode(rightEchoPin, INPUT);
+ pinMode(middleEchoPin, INPUT);
+ pinMode(trigPin, OUTPUT);
+ digitalWrite(trigPin, LOW);
+}
+void trigger(){
+ digitalWrite(trigPin, HIGH);
+ delayMicroseconds(10);
+ digitalWrite(trigPin, LOW);
+}
+void distanceSensorUpdate(){
+  trigger();
+  unsigned long leftDuration = pulseIn(leftEchoPin, HIGH, 10000);
   leftDistance_cm = leftDuration * 0.034 / 2;
+  trigger();
   unsigned long rightDuration = pulseIn(rightEchoPin, HIGH, 10000);
   rightDistance_cm = rightDuration * 0.034 / 2;
+  trigger();
   unsigned long middleDuration = pulseIn(middleEchoPin, HIGH, 10000);
   middleDistance_cm = middleDuration * 0.034 / 2;
 }
