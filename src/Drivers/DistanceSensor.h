96384872688fbfffbff752a50ff6ee81ca804270
imbluedabr <baldergroen@gmail.com>
2025-10-29 15:10:37 +0100

    fixed pins

--- src/Drivers/DistanceSensor.h
+++ src/Drivers/DistanceSensor.h
@@ -1,9 +1,9 @@
 #pragma once
 #include <Arduino.h>
-#define trigPin  2
-#define leftEchoPin  3
-#define rightEchoPin  4
-#define middleEchoPin  5
+#define trigPin  8
+#define leftEchoPin  11
+#define rightEchoPin  9
+#define middleEchoPin 10
 
 extern unsigned int leftDistance_cm;
 extern unsigned int middleDistance_cm;
