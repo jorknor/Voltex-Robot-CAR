a7fa8a1fe4a10ae38e71ec63a414c777f1a96bac
jorknor <jor123456789rit@gmail.com>
2025-10-31 12:27:51 +0100

    Update LineSensor.h pin definitions to bit shifts

--- src/Drivers/LineSensor.h
+++ src/Drivers/LineSensor.h
@@ -1,13 +1,14 @@
 #pragma once
 
-#define HLS_PIN 1
-#define MLS_PIN 10
-#define MRS_PIN 100
-#define HRS_PIN 1000
+#define HLS_PIN (1<<0)
+#define MLS_PIN (1<<1)
+#define MRS_PIN (1<<2)
+#define HRS_PIN (1<<3)
 
 extern int lineSensors[4];
 void lineSensorInit();
 void lineSensorUpdate();
+
 
 
 
